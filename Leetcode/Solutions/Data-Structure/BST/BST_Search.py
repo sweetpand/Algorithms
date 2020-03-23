6fb9ec92b595f0b917b183bbae10934d5f64de65
Zham <56076483+sweetpand@users.noreply.github.com>
2020-03-23 23:39:41 +0800

    bst search
    bst search

---
+++ Leetcode/Solutions/Data-Structure/BST/BST_Search.py
@@ -0,0 +1,12 @@
+  
+#Question: Given a BST and a value to find, find the node with that value
+#Solution: Simple BST tree traversal
+#Difficulty: Easy
+
+def searchBST(root, val):
+        #As long as the current node is not null, and its value is not the value we're looking for
+        while root and val != root.val:
+            #Move to left child node if the value we're looking for is less than current nodes value, else to right child node
+            root = root.left if val < root.val else root.right
+        #Return the node we stop at
+        return root
