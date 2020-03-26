d8b19d0f020c150664f5c8ca5fb233e9fc5c4e3f
Zham <56076483+sweetpand@users.noreply.github.com>
2020-03-27 00:18:13 +0800

    Find_Pivot_Index
    Find_Pivot_Index

---
+++ Leetcode/Solutions/Find_Pivot_Index.py
@@ -0,0 +1,19 @@
+# Question: Find the index of the element in the list such that
+#           the sum of elements to its left equals the sum of elements to its right
+# Difficulty: Easy
+# Solution: Store the sum of the left side and right side and adjust each loop and check
+# Space Complexity: O(1)
+# Time Complexity: O(n)
+
+from typing import List
+
+def pivotIndex(self, nums: List[int]) -> int:
+        leftSum = 0
+        rightSum = sum(nums)
+        
+        for i, v in enumerate(nums):
+            rightSum -= v     
+            if leftSum == rightSum: return i
+            leftSum += v
+            
+        return -1
