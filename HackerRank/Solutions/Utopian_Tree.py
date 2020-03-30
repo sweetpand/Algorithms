b9ae79048b92b8b4433875906c65e25f8e3ce55e
Zham <56076483+sweetpand@users.noreply.github.com>
2020-03-30 23:53:23 +0800

    Utopian_Tree.py
    Utopian_Tree.py

---
+++ HackerRank/Solutions/Utopian_Tree.py
@@ -0,0 +1,15 @@
+import sys
+
+
+t = int(raw_input().strip())
+for a0 in range(t):
+    n = int(raw_input().strip())
+    h = 1
+    c = 1
+    for i in range(n):
+        if c == 1:
+            h = h * 2
+        else:
+            h = h + 1
+        c *= -1
+    print h
