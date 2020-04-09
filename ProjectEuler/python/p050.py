abcf3927b5e3e89011a49bf1c2ad67dd48fbe6dc
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-10 01:23:20 +0800

    p050.py
    p050.py

---
+++ ProjectEuler/python/p050.py
@@ -0,0 +1,29 @@
+# 
+# Solution to Project Euler problem 50
+
+import eulerlib, sys
+if sys.version_info.major == 2:
+	range = xrange
+
+
+def compute():
+	ans = 0
+	isprime = eulerlib.list_primality(999999)
+	primes = eulerlib.list_primes(999999)
+	consecutive = 0
+	for i in range(len(primes)):
+		sum = primes[i]
+		consec = 1
+		for j in range(i + 1, len(primes)):
+			sum += primes[j]
+			consec += 1
+			if sum >= len(isprime):
+				break
+			if isprime[sum] and consec > consecutive:
+				ans = sum
+				consecutive = consec
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
