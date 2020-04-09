bc6d6051d2c213e2e4d15cbfaa658c199110b071
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-10 01:27:06 +0800

    p052.py
    p052.py

---
+++ ProjectEuler/python/p052.py
@@ -0,0 +1,14 @@
+# 
+# Solution to Project Euler problem 52
+
+import itertools
+
+
+def compute():
+	cond = lambda i: all(sorted(str(i)) == sorted(str(j * i)) for j in range(2, 7))
+	ans = next(i for i in itertools.count(1) if cond(i))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
