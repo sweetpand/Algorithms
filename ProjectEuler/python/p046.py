f5e762127075116b4d83487122e0abad048101f7
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-10 01:17:48 +0800

    p046.py
    p046.py

---
+++ ProjectEuler/python/p046.py
@@ -0,0 +1,28 @@
+# 
+# Solution to Project Euler problem 46
+
+import eulerlib, itertools, sys
+if sys.version_info.major == 2:
+	filterfalse = itertools.ifilterfalse
+else:
+	filterfalse = itertools.filterfalse
+
+
+def compute():
+	ans = next(filterfalse(test_goldbach, itertools.count(9, 2)))
+	return str(ans)
+
+
+def test_goldbach(n):
+	if n % 2 == 0 or eulerlib.is_prime(n):
+		return True
+	for i in itertools.count(1):
+		k = n - 2 * i * i
+		if k <= 0:
+			return False
+		elif eulerlib.is_prime(k):
+			return True
+
+
+if __name__ == "__main__":
+	print(compute())
