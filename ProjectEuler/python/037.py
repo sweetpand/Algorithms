e63676f6b1d092381f679d7317b08bd06d7fafa7
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-09 21:31:17 +0800

    037.py
    037.py

---
+++ ProjectEuler/python/037.py
@@ -0,0 +1,32 @@
+# 
+# Solution to Project Euler problem 37
+
+
+import eulerlib, itertools, sys
+if sys.version_info.major == 2:
+	filter = itertools.ifilter
+
+
+def compute():
+	ans = sum(itertools.islice(filter(is_truncatable_prime, itertools.count(10)), 11))
+	return str(ans)
+
+
+def is_truncatable_prime(n):
+	# Test if left-truncatable
+	i = 10
+	while i <= n:
+		if not eulerlib.is_prime(n % i):
+			return False
+		i *= 10
+	
+	# Test if right-truncatable
+	while n > 0:
+		if not eulerlib.is_prime(n):
+			return False
+		n //= 10
+	return True
+
+
+if __name__ == "__main__":
+	print(compute())
