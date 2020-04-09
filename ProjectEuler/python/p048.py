045ea790947b832c4ad5ce86be29b5ffde9734ff
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-10 01:21:19 +0800

    p048.py
    p048.py

---
+++ ProjectEuler/python/p048.py
@@ -0,0 +1,12 @@
+# 
+# Solution to Project Euler problem 48
+
+
+def compute():
+	MOD = 10**10
+	ans = sum(pow(i, i, MOD) for i in range(1, 1001)) % MOD
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
