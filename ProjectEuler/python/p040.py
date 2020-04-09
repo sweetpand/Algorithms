383b815e17d3fdf9a65931cb2f23081e0f765d9a
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-09 21:32:12 +0800

    p040.py
    p040.py

---
+++ ProjectEuler/python/p040.py
@@ -0,0 +1,18 @@
+# 
+# Solution to Project Euler problem 40
+
+import sys
+if sys.version_info.major == 2:
+	range = xrange
+
+
+def compute():
+	s = "".join(str(i) for i in range(1, 1000000))
+	ans = 1
+	for i in range(7):
+		ans *= int(s[10**i - 1])
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
