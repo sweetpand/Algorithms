b254cd00ca9e9fd050448e83ff09bfc9da1254ad
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-14 21:53:55 +0800

    p347.py
    p347.py

---
+++ ProjectEuler/python/p347.py
@@ -0,0 +1,38 @@
+import eulerlib
+
+
+def compute():
+	LIMIT = 10000000
+	
+	possible = set()
+	primes = eulerlib.list_primes(LIMIT // 2)
+	end = eulerlib.sqrt(LIMIT)
+	for i in range(len(primes)):
+		p = primes[i]
+		if p > end:
+			break
+		for j in range(i + 1, len(primes)):
+			q = primes[j]
+			lcm = p * q
+			if lcm > LIMIT:
+				break
+			multlimit = LIMIT // lcm
+			
+			multiplier = 1
+			while multiplier * p <= multlimit:
+				multiplier *= p
+			maxmult = multiplier
+			while multiplier % p == 0:
+				multiplier //= p
+				while multiplier * q <= multlimit:
+					multiplier *= q
+				maxmult = max(multiplier, maxmult)
+			possible.add(maxmult * lcm)
+	
+	ans = sum(possible)
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
+	
