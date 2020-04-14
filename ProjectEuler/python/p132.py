e013e49fa297b8a04dbb32d2569b56ff5f96068f
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-14 22:02:49 +0800

    p132.py
    p132.py

---
+++ ProjectEuler/python/p132.py
@@ -0,0 +1,17 @@
+import eulerlib, itertools
+
+
+def compute():
+	# Among the integers starting from 2, take the sum of
+	# the first 40 integers satisfying the filter condition
+	cond = lambda i: eulerlib.is_prime(i) and repunit_mod(10**9, i) == 0
+	ans = sum(itertools.islice(filter(cond, itertools.count(2)), 40))
+	return str(ans)
+
+
+def repunit_mod(k, m):
+	return (pow(10, k, m * 9) - 1) // 9
+
+
+if __name__ == "__main__":
+	print(compute())
