0a755343c43d842dfcff18d94d4ad61cb26ab1c8
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-21 17:01:05 +0800

    _1364.sql
    _1364.sql

---
+++ Leetcode/Solutions/SQL/1364	 Number of Trusted Contacts of a Customer/_1364.sql
@@ -0,0 +1,12 @@
+select i.invoice_id,
+c.customer_name,
+i.price,
+count(cont.contact_name) contacts_cnt,
+sum(
+    if(cont.contact_name in (select distinct customer_name from customers), 1, 0)
+) as trusted_contacts_cnt
+from invoices i
+join customers c on c.customer_id = i.user_id
+left join Contacts cont on cont.user_id = c.customer_id
+group by i.invoice_id
+order by i.invoice_id;
